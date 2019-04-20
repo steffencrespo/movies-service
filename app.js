602f1bfb32fc1136f7aea2f1d7ff301f1fe12fad
Leonardo Steffen <leonardo_steffen@gap.com>
2019-04-20 15:28:17 -0700

    starting the works

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json(['welcome to the movieDB; /movies']);
+});
+
+app.get('/movies', (req, res) => {
+  res.json(['one', 'two']);
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
