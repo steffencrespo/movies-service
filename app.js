c857f2ba20155bd3de9489675e0fd5bb615d06a2
Leonardo Steffen <leonardo_steffen@gap.com>
2019-04-21 16:32:01 -0700

    feat: uses dotenv to get tmdb api key from environment

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const express = require('express');
+require('dotenv/config');
 
+const express = require('express');
+const fetch = require('node-fetch');
 const app = express();
 
 app.get('/', (req, res) => {
@@ -7,7 +9,10 @@ app.get('/', (req, res) => {
 });
 
 app.get('/movies', (req, res) => {
-  res.json(['one', 'two', 'three']);
+  const popularMoviesUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=1`;
+  fetch(popularMoviesUrl)
+    .then(res => res.json(res))
+    .then(json => res.json(json));
 });
 
 app.listen(3000, () => {
